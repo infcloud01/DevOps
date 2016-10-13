ff8d88c087922b08e4325574e81511921e5b5c2b
infcloud01 <infcloud01@gmail.com>
2016-10-12 22:51:36 -0700

    Added a simple function to get-services in a chosen state

--- Setup-Env.ps1
+++ Setup-Env.ps1
@@ -4,3 +4,9 @@ cd C:\Users\206404804\Dropbox (Personal)\Service Delivery\2016\Architecture
 New-Alias grep findstr
 New-Alias gsrv Get-Service
 
+#Enter a paramter of Running or Stopped as the argument when you run this to get a list of services in that state
+function srv () {
+   get-service | Where-Object Status -EQ $args[0] 
+}
+
+
--- srv.ps1
+++ srv.ps1
@@ -1,2 +1,5 @@
 #Enter a paramter of Running or Stopped as the argument when you run this to get a list of services in that state
-get-service | Where-Object Status -EQ $args[0]
+function srv () {
+   get-service | Where-Object Status -EQ $args[0] 
+}
+
