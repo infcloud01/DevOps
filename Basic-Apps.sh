77ab51d428f36029fc8065b38174de035dae5225
root <infcloud01@gmail.com>
2016-10-15 05:41:25 +0000

    Add '-y' to the poweshell install

--- Basic-Apps.sh
+++ Basic-Apps.sh
@@ -3,7 +3,7 @@ yum install -y vim
 #Install git
 yum install -y git
 #Install powershell
-sudo yum install https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.10/powershell-6.0.0_alpha.10-1.el7.centos.x86_64.rpm
+sudo yum install -y https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-alpha.10/powershell-6.0.0_alpha.10-1.el7.centos.x86_64.rpm
 #Install puppetlabs
 sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 sudo yum install -y puppetserver
