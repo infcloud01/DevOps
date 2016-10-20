817326d4bfcf5efc2c217dac01dc91e76ca44ca7
infcloud01 <infcloud01@gmail.com>
2016-10-20 08:17:54 -0700

    Minor update to the description

--- user-add.pp
+++ user-add.pp
@@ -1,9 +1,3 @@
-# THE OLD WAY
-# /usr/sbin/groupadd -g 2050 sscope2
-#/usr/sbin/groupadd -g 902 omsuser
-#/usr/sbin/useradd –g 2050 –u 2050 –s /bin/bash –m –d /home/sscope2 sscope2
-#/usr/sbin/useradd –g 902 –u 902 –s /bin/bash –m –d /home/omsuser omsuser
-
 #Add users below to create
 user { ['infcloud01']  :
   ensure     => 'present',
--- user.pp
+++ user.pp
@@ -1,5 +1,17 @@
-user { 'infcloud01' :
-  managehome => true,
-  uid        => '2016',
+user { ['infcloud01']  :
   ensure     => 'present',
+  uid        => '1996',
+  managehome => true,
+  groups     => [ 'infcloud01'],
+  password   => pw_hash('ChangeM3', 'SHA-512','random'),
+  gid        => '1996',
+  shell      => '/bin/bash',
 }
+user { [ 'infcloud02' ] :
+  ensure     => 'present',
+  uid        => '1978',
+  managehome => true,
+  groups     => ['infcloud02'],
+  password   => pw_hash('ChangeM3', 'SHA-512','random'),
+  gid        => '1978',
+  shell      => '/bin/bash',
