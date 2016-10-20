8042e55d15e04317838f2e56d388cb888d3de418
root <root@ip-172-31-13-212.us-west-2.compute.internal>
2016-10-20 02:55:48 -0400

    Puppet manifest to create a sudo file

--- sudo.pp
+++ sudo.pp
@@ -3,8 +3,6 @@ $sudofile = '#Managed by Puppet
 root    ALL=(ALL)       ALL
 %wheel  ALL=(ALL)       ALL
 ec2-user        ALL=(ALL)       NOPASSWD: ALL 
-%ADL-SR_NBCU-IT_WEB_UNIX_Localadmin ALL=(ALL) NOPASSWD: ALL
-omsuser ALL=(ALL)       NOPASSWD: ALL
 '
 
 file { '/etc/sudoers' :
