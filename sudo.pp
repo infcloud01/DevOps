cc766fc539cd41b634c11f8dfc9062a8a4ef436c
infcloud01 <infcloud01@gmail.com>
2016-10-20 00:06:29 -0700

    Minor update to description

--- sudo.pp
+++ sudo.pp
@@ -1,10 +1,11 @@
+# Define a variable with the standard text for a sudoers file
 $sudofile = '#Managed by Puppet
 #Allow root to run any commands anywhere
-root    ALL=(ALL)       ALL
-%wheel  ALL=(ALL)       ALL
-ec2-user        ALL=(ALL)       NOPASSWD: ALL 
+root                                      ALL=(ALL)       ALL
+%wheel                                    ALL=(ALL)       ALL
+ec2-user                                  ALL=(ALL)       NOPASSWD: ALL  
 '
-
+#Create the file with the text from the variable above
 file { '/etc/sudoers' :
   ensure  => 'file',
   content => $sudofile,
