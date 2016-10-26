215617632841455f518b83ea4de8b704b084522e
infcloud01 <infcloud01@gmail.com>
2016-10-25 21:44:25 -0700

    Initial file

---
+++ remove-user.ps1
@@ -0,0 +1,4 @@
+#Powershell script to remove a user from a remote computer's local group
+#########################################################################
+$Computer = $env:COMPUTERNAME
+$ADSI = [ADSI] ("WinNT://$Computer")
