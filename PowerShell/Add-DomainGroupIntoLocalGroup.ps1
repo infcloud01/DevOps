333f462f01e943eeb1da883f021833edf4d384a1
infcloud01 <infcloud01@gmail.com>
2016-10-29 22:20:11 -0700

    Initial File

--- PowerShell/Add-DomainGroupIntoLocalGroup.ps1
+++ PowerShell/Add-DomainGroupIntoLocalGroup.ps1
@@ -1,11 +1,13 @@
 <# Add a domain group onto a remote server
 source: https://gallery.technet.microsoft.com/scriptcenter/d4cd8c1e-7073-42af-8c7c-89e19334f979 
 #>
+#MyChanges: Edit the domain group name, change the $server variable to an input, change the remote group,
 #Initaliaze the Domain Group Object
-$DomainGroup = [ADSI]"WinNT://DOMAIN/Citrix Desktop Users,group"
+$server = $args[0]
+#Get the domain group name
+$GroupName = $args[1]
+$DomainGroup =[ADSI]"WinNT://$env:USERDOMAIN/$GroupName,group"
 #Get Local Group object 
-$LocalGroup = [ADSI]"WinNT://$Server/Remote Desktop Users,group"
-#Assign DomainGroup to LocalGroup 
-$LocalGroup.Add($DomainGroup.Path)  
-
-
+$LocalGroup=[ADSI]"WinNT://$server/MyGroup,group"
+#Assign DomainGroup to LocalGroup
+$LocalGroup.Add($DomainGroup.Path)
