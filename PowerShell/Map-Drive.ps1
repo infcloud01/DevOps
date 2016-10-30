0dea1b35dbb7e19daaceef41bf320f0c1387e87a
Conrad <infcloud01@gmail.com>
2016-10-29 22:25:20 -0700

    Put in a generic server path

--- PowerShell/Map-Drive.ps1
+++ PowerShell/Map-Drive.ps1
@@ -1,6 +1,6 @@
 #Map a drive
 <<<<<<< HEAD
-New-PSDrive -Persist -Name 'I' -PSProvider 'FileSystem' -Root "\\ushapwp00029\pxestore\BL_Software" 
+New-PSDrive -Persist -Name 'I' -PSProvider 'FileSystem' -Root "\\server\path" 
 =======
 New-PSDrive -Persist -Name 'I' -PSProvider "FileSystem" -Root "\\WindowsShare" 
 >>>>>>> origin/master
