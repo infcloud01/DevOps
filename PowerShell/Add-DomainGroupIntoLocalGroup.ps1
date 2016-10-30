f08279703a9aef6f33ef0a0ad63c3ff2d364dea7
infcloud01 <infcloud01@gmail.com>
2016-10-30 10:31:34 -0700

    Minor comment changes

--- PowerShell/Add-DomainGroupIntoLocalGroup.ps1
+++ PowerShell/Add-DomainGroupIntoLocalGroup.ps1
@@ -2,12 +2,12 @@
 source: https://gallery.technet.microsoft.com/scriptcenter/d4cd8c1e-7073-42af-8c7c-89e19334f979 
 #>
 #MyChanges: Edit the domain group name, change the $server variable to an input, change the remote group,
-#Use the command line parameters for the server and group name
+#Get the server name from the command line
 $server = $args[0]
+#Get the domain group name from the command line
 $GroupName = $args[1]
-#Get the domain group object
 $DomainGroup =[ADSI]"WinNT://$env:USERDOMAIN/$GroupName,group"
-#Get the Local Group object 
-$LocalGroup=[ADSI]"WinNT://$server/MyGroup,group" #Change MyGroup to your local group name
-#Assign the DomainGroup to the LocalGroup
+#Get Local Group object 
+$LocalGroup=[ADSI]"WinNT://$server/MyGroup,group" #Chage MyGroup to your local group
+#Assign DomainGroup to LocalGroup
 $LocalGroup.Add($DomainGroup.Path)
