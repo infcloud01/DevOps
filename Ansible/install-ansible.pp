c33ca8fa14edf43d6ee8bf4fd0e12812d0da2381
infcloud01 <infcloud01@gmail.com>
2016-11-19 00:16:21 -0500

    Updated instructions

--- Ansible/install-ansible.pp
+++ Ansible/install-ansible.pp
@@ -1,5 +1,6 @@
-package { 'epel-release':
-}
+#The epel-package works on centos but not rhel; install the epel from rpm if you're using redhat then run the puppet file
+#package { 'epel-release':
+#}
 package { 'python':
 }
 package { 'ansible':
