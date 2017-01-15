9eae75b322276010ff73c40d8947091c63b90fb5
infcloud01 <infcloud01@gmail.com>
2017-01-15 14:27:27 -0800

    Initial File

---
+++ PowerShell/DSC-Scratch.ps1
@@ -0,0 +1,13 @@
+Configuration audio{
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string[]]$ComputerName
+  )
+  node $ComputerName {
+    Service audiosrv {
+      Name='audiosrv'
+      State='running'
+    }
+  }
+}
+audio -ComputerName $env:COMPUTERNAME -OutputPath  C:\dsc
