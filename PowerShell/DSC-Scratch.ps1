00e51e91db62ed4fec35a8406e1913f658608d63
infcloud01 <infcloud01@gmail.com>
2017-01-15 22:43:45 -0800

    Added IIS feature

--- PowerShell/DSC-Scratch.ps1
+++ PowerShell/DSC-Scratch.ps1
@@ -13,3 +13,30 @@ Configuration audio{
 audio -ComputerName $env:COMPUTERNAME -OutputPath  C:\dsc
 
 Start-DscConfiguration -path . -wait -verbose
+
+Configuration ExampleConfig{
+  Import-DscResource -Module nx
+
+  Node "centos"{
+    nxFile ExampleFile{
+        DesintationPath = "/tmp/example"
+        Contents = "hello world \n"
+        Ensure = "Present"
+        Type = "File"
+    }
+  }
+}
+
+ExampleConfig -OutputPath:"C:\DSC"
+
+Configuration IIS{
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+  Node "$env:COMPUTERNAME"{
+    WindowsFeature IIS{
+        Ensure = 'present'
+        Name = 'Web-Server'
+    }
+   }
+   }
+IIS -OutputPath:"C:\dsc"
