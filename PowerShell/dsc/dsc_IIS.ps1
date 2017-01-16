847f31014c1fb5413e8415097fe58e2cc4d73154
Conrad <infcloud01@gmail.com>
2017-01-15 23:15:54 -0800

    Modified user

---
+++ PowerShell/dsc/dsc_IIS.ps1
@@ -0,0 +1,11 @@
+﻿Configuration IIS{
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+  Node "$env:COMPUTERNAME"{
+    WindowsFeature IIS{
+        Ensure = 'present'
+        Name = 'Web-Server'
+    }
+   }
+  }
+IIS -OutputPath:"C:\Users\username\Documents\GitHub\DevOps\PowerShell\dsc"
